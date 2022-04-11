d04c785171175160c4d461fbefc5575683b9fcec
Alan Phil <filippov.pda@gmail.com>
2022-04-11 16:42:47 +0400

    DS day 4 tasks with tests

---
+++ src/test/java/DataStructure/Day4/PascalsTriangleTest.java
@@ -0,0 +1,25 @@
+package DataStructure.Day4;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PascalsTriangleTest {
+
+    @Test
+    void generateTest() {
+        int numRows = 7;
+        List<List<Integer>> expected = new PascalsTriangle().generate(numRows);
+        assertEquals(List.of(List.of(1),
+                List.of(1, 1),
+                List.of(1, 2, 1),
+                List.of(1, 3, 3, 1),
+                List.of(1, 4, 6, 4, 1),
+                List.of(1, 5, 10, 10, 5, 1),
+                List.of(1, 6, 15, 20, 15, 6, 1)), expected);
+    }
+}
