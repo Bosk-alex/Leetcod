cece5826b87608da6b27c3ee2418fe92a81fa73d
Alan Phil <filippov.pda@gmail.com>
2022-04-17 18:17:58 +0400

    Alg day 7 with tests

--- src/main/java/algorithm/day_7/MaxIslandArea.java
+++ src/main/java/algorithm/day_7/MaxIslandArea.java
@@ -1,5 +1,36 @@
 package algorithm.day_7;
 
 public class MaxIslandArea {
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
 
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 2) continue;
+                if (grid[row][col] == 1) max = Math.max(max,dfs(row, col, grid));
+            }
+        }
+
+        return max;
+    }
+
+    private int dfs(int sr, int sc, int[][] image) {
+        int area = 1;
+        image[sr][sc] = 2;
+
+        if (isValid(sr - 1, sc, image)) area += dfs(sr - 1, sc, image);
+        if (isValid(sr, sc + 1, image)) area += dfs(sr, sc + 1, image);
+        if (isValid(sr + 1, sc, image)) area += dfs(sr + 1, sc, image);
+        if (isValid(sr, sc - 1, image)) area += dfs(sr, sc - 1, image);
+
+        return area;
+    }
+
+    private boolean isValid(int row, int col, int[][] image) {
+        return (row >= 0 &&
+                row < image.length &&
+                col >= 0 &&
+                col < image[0].length &&
+                image[row][col] == 1);
+    }
 }
---
+++ src/test/java/algorithm/day_7/MaxIslandAreaTest.java
@@ -0,0 +1,28 @@
+package algorithm.day_7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxIslandAreaTest {
+
+    @Test
+    void maxAreaOfIslandTest() {
+        int[][] grid = new int[][]{{0, 0, 0, 0, 0, 0, 0, 0}};
+        int expected = 0;
+/*
+        int[][] grid = new int[][]{
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+        int expected = 6;
+*/
+        assertEquals(expected, new MaxIslandArea().maxAreaOfIsland(grid));
+    }
+}
